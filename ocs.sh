9bcea6a35142319c048845544309c0bf7666c1b3
Robson Vaamonde <robsonvaamonde@gmail.com>
2016-08-24 15:53:27 -0300

    Create ocs.sh

---
+++ ocs.sh
@@ -0,0 +1,5 @@
+apt-get install make apache2 mysql-server php perl libapache2-mod-perl2 libapache2-mod-php snmp
+apt-get install libio-compress-perl libxml-simple-perl libdbi-perl libdbd-mysql-perl 
+apt-get install libapache-dbi-perl libsoap-lite-perl libnet-ip-perl 
+apt-get install php-mysql php-gd php7.0-dev php-mbstring php-soap
+perl -MCPAN -e 'install XML::Entities'
