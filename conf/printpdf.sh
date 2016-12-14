1ce3dba53e159d7962c61ff8cdf3dc95daaa4fd9
Robson Vaamonde <robsonvaamonde@gmail.com>
2016-12-14 20:04:26 -0200

    Update printpdf.sh

--- conf/printpdf.sh
+++ conf/printpdf.sh
@@ -1,10 +1,24 @@
 #!/bin/sh
-# Shell script to print to PDF files
+# Modificado por: Robson Vaamonde
+# Autor original: BjTechNews
+# Site: https://bjtechnews.org/2012/02/08/pdf-printer-on-a-samba-server/
+# Site: www.procedimentosemti.com.br
+# Facebook: facebook.com/ProcedimentosEmTI
+# Facebook: facebook.com/BoraParaPratica
+# YouTube: youtube.com/BoraParaPratica
+# Data de criação: 31/05/2016
+# Data de atualização:09/08/2016
+# Versão: 0.4
+# Testado e homologado para a versão do Ubuntu Server 16.04 LTS x64
+# Kernel >= 4.4.x
+
+# Opções dos parametros do Shell Script para Impressão do Arquivo em PDF
 # Parameters $1 = spool file (smbprn . . . )
 # $2 = user name
 # $3 = user home directory
 # $4 = print job name
-OUTDIR =/srv/samba/pdf
-  echo Converting $1 to “$4” for user $2 in $3 >> pdfprint.log
+
+OUTDIR =/arquivos/pti.intra/pdf
+  echo Convertendo $1 para “$4” for user $2 in $3 >> /var/log/cups/pdfprint.log
   ps2pdf $1 “$OUTDIR/$4.pdf”
 rm $1
