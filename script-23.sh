1c283ecf0a0303c708c637ed5e88fb1687dbedce
Robson Vaamonde <robsonvaamonde@gmail.com>
2017-05-24 09:03:44 -0300

    Update script-23.sh

--- script-23.sh
+++ script-23.sh
@@ -5,7 +5,7 @@
 # Facebook: facebook.com/BoraParaPratica
 # YouTube: youtube.com/BoraParaPratica
 # Data de criação: 13/03/2017
-# Data de atualização: 13/03/2017
+# Data de atualização: 24/05/2017
 # Versão: 0.8
 # Testado e homologado para a versão do Ubuntu Server 16.04 LTS x64
 # Kernel >= 4.4.x
