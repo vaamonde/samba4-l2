9fbf28c7084bb2834d57776f626c1c0fd5d88095
Robson Vaamonde <robsonvaamonde@gmail.com>
2018-09-27 00:18:03 -0300

    Update script-03.sh

--- script-03.sh
+++ script-03.sh
@@ -5,15 +5,15 @@
 # Facebook: facebook.com/BoraParaPratica
 # YouTube: youtube.com/BoraParaPratica
 # Data de criação: 31/05/2016
-# Data de atualização: 13/03/2017
-# Versão: 0.8
+# Data de atualização: 27/09/2018
+# Versão: 0.9
 # Testado e homologado para a versão do Ubuntu Server 16.04 LTS x64
 # Kernel >= 4.4.x
 #
 # Instalação dos pacotes principais para a quarta etapa, indicado para a distribuição GNU/Linux Ubuntu Server 16.04 LTS x64
 #
 # Instalando as dependências para Webmin - WebAdmin
-# Baixando o Webmin do site Oficial (versão: 1.801)
+# Baixando o Webmin do site Oficial (versão: 1.890-1)
 # Instalando o Webmin via dpkg
 # Porta padrão de acesso ao Webmin: https://SEU_ENDEREÇO_IP:10000
 #
@@ -45,7 +45,7 @@ then
 				then		
 					 clear
 					 # Versão do Webmin para ser feito download
-					 VERSAO="webmin_1.831_all.deb"
+					 VERSAO="webmin_1.890_all.deb"
 					 TAMANHO="14.8 MB"
 					 #
 					 echo -e "Usuário é `whoami`, continuando a executar o Script-03.sh"
