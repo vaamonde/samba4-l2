147369231681c7267f70ff3d0fd0f3be934af15c
Robson Vaamonde <robsonvaamonde@gmail.com>
2018-10-02 14:17:10 -0300

    Update script-24.sh

--- script-24.sh
+++ script-24.sh
@@ -35,7 +35,11 @@ then
 					 #
 					 #Variaveis de ambiente para o script
 					 #
-           
+					 USER="root"
+					 PASSWORD="pti@2016"
+					 RSYSLOG="rsyslog"
+					 LOGANALYZER="loganalyzer-4.1.6.tar.gz"
+					 
 					 echo -e "Usuário é `whoami`, continuando a executar o Script-23.sh"
 					 echo
 					 echo -e "Rodando o Script-24.sh em: `date`" > $LOG
@@ -43,17 +47,98 @@ then
 					 echo -e "                     Instalação do LogAnalyzer"
 					 echo -e "================================================================================="
 					 echo
-					 echo -e "Pressione <Enter> para iniciar as configurações"
+					 echo -e "Pressione <Enter> para iniciar a instalação e configuração"
 					 read
 					 sleep 2
 					 clear
 					 
-					 echo -e "Copiando o arquivo firewall.sh, aguarde..."
-					 cp -v conf/firewall /etc/init.d/ >> $LOG
-					 echo -e "Arquivo copiado com sucesso!!!"
-					 echo
+					 echo -e "Criando a base de dados do Rsyslog e setando as permissões, aguarde..."
+					 #Criando a variável da criação da Base de Dados do Rsyslog
+					 #Variaveis utilizada pelo MySQL para a criação do Bando de Dados
+					 DATABASE1="CREATE DATABASE syslog;"
+					 USERDATABASE1="CREATE USER 'syslog' IDENTIFIED BY 'syslog';"
+					 GRANTDATABASE1="GRANT USAGE ON *.* TO 'syslog' IDENTIFIED BY 'syslog';"
+					 GRANTALL1="GRANT ALL PRIVILEGES ON syslog.* TO 'syslog';"
+					 FLUSH1="FLUSH PRIVILEGES;"
+					 #Criando o Banco de Dados e setando as informações de usuários e senha utilizando o comando mysql
+					 mysql -u $USER -p$PASSWORD -e "$DATABASE1" mysql &>> $LOG
+					 mysql -u $USER -p$PASSWORD -e "$USERDATABASE1" mysql &>> $LOG
+					 mysql -u $USER -p$PASSWORD -e "$GRANTDATABASE1" mysql &>> $LOG
+					 mysql -u $USER -p$PASSWORD -e "$GRANTALL1" mysql &>> $LOG
+					 mysql -u $USER -p$PASSWORD -e "$FLUSH1" mysql &>> $LOG
+					 echo -e "Base de dados do Rsyslog criada com sucesso!!!"
 					 sleep 2
+					 echo
 					 
+					 echo -e "Criando a base de dados do LogAnalyzer e setando as permissões, aguarde..."
+					 #Criando a variável da criação da Base de Dados do LogAnalyzer
+					 #Variaveis utilizada pelo MySQL para a criação do Bando de Dados
+					 USER="root"
+					 DATABASE2="CREATE DATABASE loganalyzer;"
+					 USERDATABASE2="CREATE USER 'loganalyzer' IDENTIFIED BY 'loganalyzer';"
+					 GRANTDATABASE2="GRANT USAGE ON *.* TO 'loganalyzer' IDENTIFIED BY 'loganalyzer';"
+					 GRANTALL2="GRANT ALL PRIVILEGES ON loganalyzer.* TO 'loganalyzer';"
+					 FLUSH2="FLUSH PRIVILEGES;"
+					 #Criando o Banco de Dados e setando as informações de usuários e senha utilizando o comando mysql
+					 mysql -u $USER -p$PASSWORD -e "$DATABASE2" mysql &>> $LOG
+					 mysql -u $USER -p$PASSWORD -e "$USERDATABASE2" mysql &>> $LOG
+					 mysql -u $USER -p$PASSWORD -e "$GRANTDATABASE2" mysql &>> $LOG
+					 mysql -u $USER -p$PASSWORD -e "$GRANTALL2" mysql &>> $LOG
+					 mysql -u $USER -p$PASSWORD -e "$FLUSH2" mysql &>> $LOG
+					 echo -e "Base de dados do LogAnalyzer criada com sucesso!!!"
+					 sleep 2
+					 echo					 
+					 
+					 echo -e "Listando os Bancos de Dados criado do Rsyslog e LogAnalyzer, aguarde..."
+					 #Criando a variável para exibição da Base de Dados do Rsyslog e LogAnalyzer
+					 SHOWSQL="SHOW DATABASES;"
+					 echo
+					 #Listando a Base de Dados criada do Rsyslog e LogAnalyzer
+					 mysql -u $USER -p$PASSWORD -e "$SHOWSQL" mysql
+					 echo
+					 echo -e "Base de dados listadas com sucesso!!!"
+					 echo
+					 echo -e "Criação da Base de Dados do Rsyslog e LogAnalyzer feita com sucesso!!!, pressione <Enter> para continuar"
+					 read
+					 sleep 2
+					 clear
+					 
+					 echo -e "Atualizando os arquivos de Configuração do Rsyslog, aguarde..."
+					 #Importando as tabelas da base de dados do Rsyslog
+					 mysql -u $RSYSLOG -D syslog -p$RSYSLOG < /usr/share/dbconfig-common/data/rsyslog-mysql/install/mysql
+					 echo -e "Atualização da Base de Dadas feita com sucesso!!!, continuando o script"
+					 echo
+					 
+					 echo -e "Atualizando o arquivo de configuração do Rsyslog.conf, aguarde..."
+					 #Fazendo o backup de arquivo de configuração Rsyslog
+					 cp -v /etc/rsyslog.conf /etc/rsyslog.conf.old  >> $LOG
+					 #Atualizando o arquivo de configuração Rsyslog
+					 cp -v conf/rsyslog.conf /etc/rsyslog.conf  >> $LOG
+					 #Atualizando o arquivo de configuração do mysql
+					 cp -v conf/mysql.conf /etc/rsyslog.d/mysql.conf >> $LOG
+					 #Reinicializando o serviço do Rsyslog
+					 sudo service rsyslog restart
+					 echo
+					 echo -e "Atualização dos arquivos de configuração do Rsyslog feita com sucesso!!!, pressione <Enter> para continuar"
+					 read
+					 sleep 2
+					 clear
+					 
+					 echo -e "Baixando, instalando e configurando o LogAnalyzer, aguarde..."
+					 #Download do LogAnalyzer
+					 wget http://download.adiscon.com/loganalyzer/$LOGANALYZER >> $LOG
+					 #Descompactando LogAnalyzer
+					 tar -xzvf $LOGANALYZER >> $LOG
+					 #Movendo o LogAnalyzer para o diretório /var/www/html/loganalyzer/
+					 mv -v $LOGANALYZER/src/* /var/www/html/loganalyzer/
+					 #Alterando as permissões do arquico config.php
+					 chmod -v 666 /var/www/html/loganalyzer/config.php
+					 #Alterando o dono e grupo do diretório loganalyzer
+					 chow -Rv www-data.www-data /var/www/html/loganalyzer/
+					 echo -e "Instalação feita com sucesso!!!, pressione <Enter> para continuar com o script"
+					 read
+					 sleep 2
+					 clear
 					 
 					 echo -e "Fim do Script-24.sh em: `date`" >> $LOG
 					 echo -e "                Finalização da Configuração do LogAnalyzer"
