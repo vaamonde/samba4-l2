1ce03247673b6b3b08d84ea7af9111332127ba7e
Robson Vaamonde <robsonvaamonde@gmail.com>
2018-10-02 13:54:49 -0300

    Update script-24.sh

--- script-24.sh
+++ script-24.sh
@@ -4,24 +4,18 @@
 # Facebook: facebook.com/ProcedimentosEmTI
 # Facebook: facebook.com/BoraParaPratica
 # YouTube: youtube.com/BoraParaPratica
-# Data de criação: 13/03/2017
-# Data de atualização: 01/10/2018
-# Versão: 0.11
+# Data de criação: 02/10/2018
+# Data de atualização: 02/10/2018
+# Versão: 0.1
 # Testado e homologado para a versão do Ubuntu Server 16.04 LTS x64
 # Kernel >= 4.4.x
 #
-# Configuração das regras de Firewall utilizando o IPTables
-# Usar o script-08.sh para analiar as portas antes da configuração dos arquivos liberação
-# Utilziar o arquivo /etc/firewall/portslibtcp para as portas liberadas do protocolo TCP
-# Utilizar o arquivo /etc/firewall/portslibudp para as portas liberadas do protocolo UDP 
-# Utilizar o arquivo /etc/firewall/portsblo para as portas bloqueadas TCP/UDP
-# Utilizar o arquivo /etc/firewall/dnsseerver para os IP do Servidores de DNS liberados
-# Utilizar o arquivo /etc/firewall/multiportslibtcp para o range de portas liberadas do protocolo TCP
+# Instalação e Configuração LogAnalyzer
 #
 # Utilizar o comando: sudo -i para executar o script
 #
-# Caminho para o Log do Script-23.sh
-LOG="/var/log/script-23.log"
+# Caminho para o Log do Script-24.sh
+LOG="/var/log/script-24.log"
 #
 # Variável da Data Inicial para calcular tempo de execução do Script
 DATAINICIAL=`date +%s`
@@ -44,9 +38,9 @@ then
            
 					 echo -e "Usuário é `whoami`, continuando a executar o Script-23.sh"
 					 echo
-					 echo -e "Rodando o Script-23.sh em: `date`" > $LOG
+					 echo -e "Rodando o Script-24.sh em: `date`" > $LOG
 					 echo -e "================================================================================="
-					 echo -e "                     Configuração da Regra de Firewall"
+					 echo -e "                     Instalação do LogAnalyzer"
 					 echo -e "================================================================================="
 					 echo
 					 echo -e "Pressione <Enter> para iniciar as configurações"
@@ -60,87 +54,17 @@ then
 					 echo
 					 sleep 2
 					 
-					 echo -e "Alterando os permissões do arquivo firewall.sh, aguarde..."
-					 chmod -v +x /etc/init.d/firewall >> $LOG
-					 echo -e "Permissões alteradas com sucesso!!!"
-					 echo
-					 sleep 2
 					 
-					 echo -e "Criando o diretório firewall em: /etc, aguarde..."
-					 mkdir -v /etc/firewall >> $LOG
-					 echo -e "Diretório criado com sucesso!!!"
-					 echo
-					 sleep 2
-					 
-					 echo -e "Copiando os arquivos de configuração do firewall, aguarde..."
-					 cp -v conf/portslibtcp /etc/firewall >> $LOG
-					 cp -v conf/portslibudp /etc/firewall >> $LOG
-					 cp -v conf/portsblo /etc/firewall >> $LOG
-					 cp -v conf/dnsserver /etc/firewall >> $LOG
-					 cp -v conf/multiportslibtcp /etc/firewall >> $LOG
-					 echo -e "Arquivos copiados com sucesso!!!"
-					 echo
-					 sleep 2
-					 
-					 echo -e "Copiando o arquivo firewall.conf, aguarde..."
-					 cp -v conf/firewall.conf /etc/rsyslog.d/ >> $LOG
-					 echo -e "Arquivo copiado com sucesso!!!"
-					 echo
-					 sleep 2
-					 
-					 echo -e "Criando o arquivo firewall.log, aguarde..."
-					 touch /var/log/firewall.log >> $LOG
-					 chown -v syslog.adm /var/log/firewall.log >> $LOG
-					 echo -e "Arquivo criado com sucesso!!!"
-					 echo
-					 sleep 2
-					 
-					 echo -e "Reinicializando o serviço do Rsyslog, aguarde..."
-					 sudo service rsyslog restart >> $LOG
-					 echo -e "Serviço Reinicializado com sucesso!!!"
-					 echo
-					 sleep 2
-					 
-					 echo -e "Atualizando as listas do apt-get, aguarde..."
-					 apt-get update >> $LOG
-					 echo -e "Listas atualziadas com sucesso!!!"
-					 echo
-					 sleep 2
-					 
-					 echo -e "Instalando o sysv-rc-conf, aguarde..."
-					 apt-get -y install sysv-rc-conf >> $LOG
-					 echo -e "sysv-rc-conf instalado com sucesso!!!"
-					 echo
-					 sleep 2
-					 
-					 echo -e "Configurando a inicialização do Firewall, pressione <Enter>"
-					 echo -e "Na tela de configuração do sysv-rc-conf, marque as opções: 2, 3, 4 e 5"
-					 echo -e "Pressione Q (quit) para sair"
-					 read
-					 sleep 2
-					 sysv-rc-conf
-					 echo -e "sysv-rc-conf atualizado com sucesso!!!"
-					 sleep 2
-					 
-					 echo -e "Inicializando o Firewall, aguarde..."
-					 echo
-					 sh /etc/init.d/firewall start
-					 echo
-					 echo
-					 echo -e "Firewall inicializado com sucesso!!!"
-					 echo
-					 sleep 2
-					 
-					 echo -e "Fim do Script-23.sh em: `date`" >> $LOG
-					 echo -e "                Finalização da Configuração da Regra de Firewall"
+					 echo -e "Fim do Script-24.sh em: `date`" >> $LOG
+					 echo -e "                Finalização da Configuração do LogAnalyzer"
 					 echo -e "================================================================================="
 					 echo
-					 # Script para calcular o tempo gasto para a execução do script-23.sh
+					 # Script para calcular o tempo gasto para a execução do script-24.sh
 						DATAFINAL=`date +%s`
 						SOMA=`expr $DATAFINAL - $DATAINICIAL`
 						RESULTADO=`expr 10800 + $SOMA`
 						TEMPO=`date -d @$RESULTADO +%H:%M:%S`
-					 echo -e "Tempo gasto para execução do script-23.sh: $TEMPO"
+					 echo -e "Tempo gasto para execução do script-24.sh: $TEMPO"
 					 echo -e "Pressione <Enter> para concluir o processo."
 					 read
 					 else
