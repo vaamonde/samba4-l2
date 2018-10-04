e9bf39d79a65cdaba7f891ff25fb1b8a048da0ee
Robson Vaamonde <robsonvaamonde@gmail.com>
2018-10-04 17:34:32 -0300

    Update script-25.sh

--- script-25.sh
+++ script-25.sh
@@ -10,7 +10,7 @@
 # Testado e homologado para a versão do Ubuntu Server 16.04 LTS x64
 # Kernel >= 4.4.x
 #
-# Configuração do PAM () integrado com Winbind e SAMBA-4
+# Configuração do PAM (Pluggable Authentication Modules for Linux) integrado com Winbind e SAMBA-4
 #
 # Utilizar o comando: sudo -i para executar o script
 #
@@ -35,6 +35,7 @@ then
 					 #
 					 #Variaveis de ambiente para o script
 					 #
+					 DOMAIN="PTI"
 					 USER="root"
 					 PASSWORD="pti@2016"
 					 ADMIN="administrator"
@@ -43,7 +44,7 @@ then
 					 echo
 					 echo -e "Rodando o Script-25.sh em: `date`" > $LOG
 					 echo -e "================================================================================="
-					 echo -e "           Configuração da Integração do PAM com Winbind e SAMBA4"
+					 echo -e "           Configuração da Integração do PAM com Winbind e SAMBA-4"
 					 echo -e "================================================================================="
 					 echo
 					 echo -e "Pressione <Enter> para iniciar a configuração"
@@ -51,8 +52,59 @@ then
 					 sleep 2
 					 clear
 					 
+					 echo -e "Atualizando as configuarações do PAM, pressione <Enter>."
+					 echo -e "Habilitar o recerso de: Create home directory on login"
+					 read
+					 sleep 2
+					 clear
+					 
+					 #Executando comando pam-auth-update
+					 pam-auth-update
+					 
+					 echo -e "Atualização feita com sucesso, pressione <Enter> para continuar com o script."
+					 read
+					 sleep 2
+					 clear
+					 
+					 echo -e "Atualizando os arquivos de configuração do PAM, pressione <Enter> para continuar"
+					 read
+					 sleep 2
+					 echo
+					 
+					 echo -e "Atualizando o arquivo de configuração COMMON-PASSWORD, aguarde..."
+					 #Atualizando o arquivo de configuração
+					 cp -v conf/common-password /etc/pam.d/common-password >> $LOG
+					 echo -e "Arquivo e atualizado com sucesso!!!, continuando o script..."
+					 echo
+					 
+					 echo -e "Reinicializando o serviço do Winbind, aguarde..."
+					 #Reinicializando o serviço
+					 sudo service winbind restart >> $LOG
+					 3cho -e "Serviços reinicializado com sucesso!!!, contiuando o script..."
+					 echo
+					 
+					 echo -e "Adicionando o usuário $ADMIN ao grupo do SUDO, aguarde..."
+					 #Alterando o grupo do usuário administrator
+					 usermod -aG sudo '$DOMAIN\$ADMIN' >> $LOG
+					 echo -e "Grupo adicionado com sucesso!!!, continuado com o script..."
+					 echo
+					 
+					 echo -e "Verificando as informação do Winbind do usuário $ADMIN, aguarde..."
+					 echo
+					 #Verificando as configurações de integração do Winbindo do usuário administrator
+					 wbinfo -i $ADMIN
+					 echo
+					 echo -e "Informações do Winbind obtidas com sucesso!!!, continuando com o script..."
+					 echo
+					 
+					 echo -e "Criando o diretório base do usuário $ADMIN, agaurde..."
+					 #Criando a estruura de diretório e acesso a pasta pessoal
+					 su - administrator >> $LOG
+					 echo -e "Diretório criado com sucesso!!!"
+					 echo
+					 
 					 echo -e "Fim do Script-25.sh em: `date`" >> $LOG
-					 echo -e "     Finalização da Configuração da Integração do PAM com Winbind e SAMBA4"
+					 echo -e "    Finalização da Configuração da Integração do PAM com Winbind e SAMBA-4"
 					 echo -e "================================================================================="
 					 echo
 					 # Script para calcular o tempo gasto para a execução do script-25.sh
